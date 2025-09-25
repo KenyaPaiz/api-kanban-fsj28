7bf5d7a61f99494dc3e3b45f59b5b82c36f109ff
KeniaKodigo <kenia.paz@kodigo.org>
2025-09-24 20:47:41 -0600

    uso de validacion de datos, obtener, crear y filtrar tareas

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,24 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreTaskRequest;
 use App\Models\Task;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
 
 class TaskController extends Controller
 {
-    //registrando una tarea (enviar datos)
-    public function store(Request $request){
+    //metodo para obtener todas las tareas
+    public function index(){
+        //SELECT t.*, u.name FROM tasks as t inner join users as u on u.id = t.user_id;
+        $tasks = Task::join('users as u','u.id','=','tasks.user_id')->select('tasks.id','tasks.title','tasks.description','tasks.status','tasks.priority','tasks.due_date','tasks.user_id', 'u.name as user')->get();
 
-        Validator::make([
-            'title' => $request->input('title'),
-            'description' => $request->input('description')
-        ], [
-            'user_id' => 'required|exists:users,id'
-        ], [
-            'user_id' => "El campo es obligatorio"
-        ]);
-        
+        return response()->json(["data" => $tasks], 200);
+    }
+
+    //registrando una tarea (enviar datos)
+    public function store(StoreTaskRequest $request){
+        //422 Unprocessable Entity
         //instancia (new) -> Task
         // $task = new Task();
         // $task->title = $request->input('title');
@@ -32,5 +32,73 @@ class TaskController extends Controller
         //insertamos una tarea de manera masiva
         Task::create($request->all()); //insert into..
         return response()->json(['message' => 'Task created successfully'], 201);
+    }
+
+    //obtener una tarea en especifico
+    public function show($taskId){
+
+        $validator = Validator::make(['task_id' => $taskId], [
+            'task_id' => 'required|exists:tasks,id'
+        ]);
+
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        $task = Task::find($taskId); //devuelve un objeto
+        return response()->json($task, 200);
+    }
+
+    //metodo para filtrar estado y prioridad de tareas
+    public function filterStatusOrPriority(Request $request){
+
+        $status = $request->query('status');
+        $priority = $request->query('priority');
+
+        $validator = Validator::make(
+            ['status' => $status, 'priority' => $priority], 
+            [
+                'status' => 'nullable|in:pendiente,en proceso,completada',
+                'priority' => 'nullable|in:baja,media,alta',
+            ]);
+
+        if($validator->fails()){
+            return response()->json(['errors' => $validator->errors()], 422);
+        }
+
+        //construyendo las consultas
+        $query = Task::query();
+
+        if($status){
+            //SELECT * FROM tasks WHERE status = "en proceso"
+            $query->where('status',$status);
+        }
+
+        if($priority){
+            //SELECT * FROM tasks WHERE priority = "alta"
+            $query->where('priority',$priority);
+        }
+
+        //devolviendo la informacion de las tareas (con o sin estado/prioridad)
+        $tasks = $query->select('title','description','status','priority','due_date','user_id')->get(); //[]
+
+        if($tasks->isEmpty()){
+            return response()->json(["message" => "Por el momento no hay tareas"], 204);
+        }
+
+        return response()->json($tasks, 200);
+    }
+
+    
+    public function update(Request $request, $taskId){
+        //encontrar la tarea en base el id
+        $task = Task::find($taskId);
+        $task->update($request->all());
+        return response()->json(["message" => "Task updated successfully"], 200);
+    }
+
+    //devolver cuantos dias falta o si se paso del limite de la fecha
+    public function remaininDays($taskId){
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -9,5 +9,14 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/v1/users', [UserController::class, 'index']);
-Route::post('/v1/users', [TaskController::class, 'store']);
+Route::prefix('v1')->group(function() {
+    Route::get('/users', [UserController::class, 'index']);
+    Route::get('/tasks', [TaskController::class, 'index']);
+    Route::post('/tasks', [TaskController::class, 'store']);
+    //ruta con queryparams (filtrando por estado y prioridad)
+    Route::get('/tasks/filter', [TaskController::class, 'filterStatusOrPriority']);
+    //agregando rutas con parametros (parametros de ruta)
+    Route::get('/tasks/{taskId}', [TaskController::class, 'show']);
+    
+});
+
